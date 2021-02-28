a3fcd90ec3cd44d9b7378c3aaf768315b4507909
ghilasjku <62773174+ghilasjku@users.noreply.github.com>
2021-02-28 22:44:20 +0100

    Update index.js

--- pages/createform/index.js
+++ pages/createform/index.js
@@ -6,11 +6,69 @@ import DemoBar from "../../components/demobar";
 
 function Index({ id }) {
 	const postUrl = `/api/findform/${id}`;
+		var items = [{
+		key: 'Header',
+		name: 'Title form',
+		icon: 'fas fa-heading',
+		static: false,
+		content: 'Placeholder Text...',
+		canHavePageBreakBefore : false,
+		canHaveAlternateForm : false,
+	   
+	  },
+	
+	  {
 
+
+ key: 'Dropdown',
+        canHaveAnswer: true,
+        name: 'Dropdown',
+        icon: 'far fa-caret-square-down',
+        label: 'Placeholder Label',
+        field_name: 'dropdown_',
+        options: [],
+	static: true,
+	  read_only:false,
+	canHaveAlternateForm : false,
+	  canHavePageBreakBefore : false,
+	  
+	   
+	  },
+		{
+		key: 'Checkboxes',
+		canHaveAnswer: true,
+		name: 'Checkboxes',
+		icon: 'far fa-check-square',
+		label: 'Placeholder Label',
+		field_name: 'checkboxes_',
+		static:false,
+		canHaveDisplayHorizontal : false,
+		options: [],
+		canHavePageBreakBefore : false,
+	  canHaveAlternateForm : false,
+	  canPopulateFromApi : false,
+		
+	  },
+	  {
+		key: 'RadioButtons',
+		canHaveAnswer: true,
+		name: 'Multiple Choice',
+		icon: 'far fa-dot-circle',
+		label: 'Placeholder Label',
+		field_name: 'radiobuttons_',
+		static: false,
+		options: [],
+		canHaveDisplayHorizontal : false,
+		canHavePageBreakBefore : false,
+	   canHaveAlternateForm : false,
+	   canPopulateFromApi : false,
+		
+	  },
+	 ];
 	return (
 		<div>
 			<DemoBar postUrl={postUrl} />
-			<FormBuilder.ReactFormBuilder saveUrl={postUrl} />
+			<FormBuilder.ReactFormBuilder toolbarItems={items} saveUrl={postUrl} />
 		</div>
 	);
 }
