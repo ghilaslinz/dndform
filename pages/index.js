3c47969237b863523102bbf63e97fd5af6522693
ghilasjku <62773174+ghilasjku@users.noreply.github.com>
2021-02-28 22:16:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Dashboard from "./dashboard/index";
-import Nav from "../components/nav/nav";
+
 const home = () => {
 	return (
 		<>
-			<Nav />
+			
 			<Dashboard />
 		</>
 	);
